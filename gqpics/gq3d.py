c9a253812f1b2f62242a90759ba5702f173d046e
ksorat <kareem.sorathia@gmail.com>
2018-01-15 17:14:51 -0500

    Lazy push message

--- gqpics/gq3d.py
+++ gqpics/gq3d.py
@@ -44,6 +44,9 @@ eBzStr = "(3.0*zRe*zRe - Radius*Radius)*(%e)*rm5"%(MagM)
 DefineScalarExpression("RadAll","polar_radius(gMesh)")
 DefineScalarExpression("Radius","if( ge(RadAll, 2.1), RadAll, 2.1)") #Respect cutout
 DefineScalarExpression("phi","cylindrical_theta(gMesh)")
+DefineScalarExpression("inRad","if( le(RadAll, 12.5), 1.0, 0.0)")
+DefineScalarExpression("outRad","if( ge(RadAll, 3.5), 1.0, 0.0)")
+DefineScalarExpression("inPhi","if( ge(abs(phi), 0.75), 1.0, 0.0)")
 
 DefineScalarExpression("xRe","coord(gMesh)[0]")
 DefineScalarExpression("yRe","coord(gMesh)[1]")
@@ -65,8 +68,12 @@ DefineScalarExpression("jScl","zonal_constant(gMesh,%e)"%(gB0/gX0))
 DefineScalarExpression("Current","abs(jScl*Jy)")
 
 #Earthward velocity
+DefineScalarExpression("vCut","inPhi*inRad*outRad")
 DefineScalarExpression("vScl","zonal_constant(gMesh,100.0)")
-DefineScalarExpression("vScl*(Vx*cos(phi)+Vy*sin(phi))")
+DefineScalarExpression("Ve","vScl*(Vx*cos(phi)+Vy*sin(phi))")
+DefineVectorExpression("Vxy","vCut*vScl*{Vx,Vy,0.0}")
+DefineVectorExpression("V"  ,"vCut*vScl*{Vx,Vy,Vz}")
+
 #Open database
 OpenDatabase(gdata)
 md0 = GetMetaData(gdata)
@@ -130,6 +137,30 @@ tOp = GetOperatorOptions(1)
 tOp.radiusFractionBBox = 0.00025
 SetOperatorOptions(tOp)
 
+#Add velocity arrows
+AddPlot("Vector","Vxy")
+pOp = GetPlotOptions()
+pOp.minFlag = 1
+pOp.maxFlag = 1
+pOp.min = 0.0
+pOp.max = 200.0
+pOp.colorTableName = "Cool"
+pOp.autoScale = 0
+pOp.scaleByMagnitude = 0
+pOp.lineStem = 0
+pOp.nVectors = 2600
+#pOp.scale = 0.025
+pOp.scale = 1
+pOp.geometryQuality = 1
+SetPlotOptions(pOp)
+
+AddOperator("Slice",0)
+ops = GetOperatorOptions(0)
+ops.project2d = 0
+ops.axisType = 2
+SetOperatorOptions(ops)
+
+
 #Set visual range
 v3d = GetView3D()
 v3d.viewNormal = (-0.128, 0.922099, -0.365171)
@@ -153,30 +184,51 @@ SetView3D(v3d)
 
 
 #Move legends
-#dBz
 fH = 0.025
+dx = 0.01
+dy = 0.01
+xScl = 0.65
+yScl = 0.45
+#dBz
+x1 = 0.15
+y1 = 0.15
 P1 = GetAnnotationObject("Plot0001")
 P1.drawMinMax = 0
 P1.managePosition = 0
-P1.position = (0.05,0.1)
+P1.position = (x1,y1)
 P1.drawTitle = 0
 P1.orientation = 2
-P1.yScale = 0.5
+P1.yScale = yScl
 P1.fontHeight = fH
-pyv.genTit("Residual Field [nT]",Pos=(0.05,0.105),height=fH)
+pyv.genTit("Residual Field [nT]",Pos=(x1,y1+dy),height=fH)
 
 #Pressure
 P2 = GetAnnotationObject("Plot0002")
+x2 = 0.9
+y2 = 0.9
 P2.drawMinMax = 0
 P2.managePosition = 0
-P2.position = (0.9,0.9)
+P2.position = (x2,y2)
 P2.drawTitle = 0
 P2.orientation = 1
-P2.xScale = 0.75
+P2.xScale = xScl
 P2.yScale = 1.0
 P2.fontHeight = fH
-pyv.genTit("Pressure",Pos=(0.875,0.925),height=fH)
+pyv.genTit("Pressure",Pos=(x2-3*dx,y2+2*dy),height=fH)
 
+#Speed
+P4 = GetAnnotationObject("Plot0004")
+x4 = 0.65
+y4 = y1
+
+P4.drawMinMax = 0
+P4.managePosition = 0
+P4.position = (x4,y4)
+P4.drawTitle = 0
+P4.orientation = 2
+P4.yScale = yScl
+P4.fontHeight = fH
+pyv.genTit("Speed [km/s]",Pos=(x4,y4+dy),height=fH)
 
 #GetAnnotationObject('Plot0000').drawMinMax = 0
 #GetAnnotationObject('Plot0000').managePosition = 0
